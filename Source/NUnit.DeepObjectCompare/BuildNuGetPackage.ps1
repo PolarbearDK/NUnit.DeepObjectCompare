a68db0459af204bd264a5e6eb2f0cd904f304a8b
PolarbearDK <philip@dsdevelopment.dk>
2016-09-29 14:38:42 +0200

    Add more targets frameworks (4.5, 4.5.2, 4.6) and bump version to 1.0.1

--- Source/NUnit.DeepObjectCompare/BuildNuGetPackage.ps1
+++ Source/NUnit.DeepObjectCompare/BuildNuGetPackage.ps1
@@ -7,10 +7,14 @@ Set-StrictMode -Version latest
 $ErrorActionPreference = "Stop"
 
 if($pack) {
-	& msbuild NUnit.DeepObjectCompare.csproj /p:Configuration=release
+	& msbuild NUnit.DeepObjectCompare.csproj /p:Configuration="Release 4.5"
+	if(!$?){throw "msbuild returned exit code $LASTEXITCODE"}
+	& msbuild NUnit.DeepObjectCompare.csproj /p:Configuration="Release 4.5.2"
+	if(!$?){throw "msbuild returned exit code $LASTEXITCODE"}
+	& msbuild NUnit.DeepObjectCompare.csproj /p:Configuration="Release 4.6"
 	if(!$?){throw "msbuild returned exit code $LASTEXITCODE"}
 	
-	& ..\.NuGet\NuGet.exe pack NUnit.DeepObjectCompare.csproj -prop Configuration=release
+	& ..\.NuGet\NuGet.exe pack NUnit.DeepObjectCompare.csproj -Verbosity detailed -Properties Configuration="Release 4.5.2"
 	if(!$?){throw "NuGet returned exit code $LASTEXITCODE"}
 }
 
